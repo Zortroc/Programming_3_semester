83a4ce1c84deae34e8603ca1d48c635a46f13467
Zortroc <zaharovilya2007@gmail.com>
2026-09-16 20:48:18 +0700

    создал библиотеку классов Model и класс внутри него Student

---
+++ 1_laba/DecanatPRO/Model/Student.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class Student
+    {
+        public string Name { set; get; }
+        public string Speciality { set; get; }
+        public string Group { set; get; }
+    }
+}
