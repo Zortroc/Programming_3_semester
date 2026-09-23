e3178292fcbf07cdad4202127286562b1e97edef
Zortroc <zaharovilya2007@gmail.com>
2026-09-23 19:29:06 +0700

    прописал мейн в програме

--- 1_laba/DecanatPRO/ConsoleView/Program.cs
+++ 1_laba/DecanatPRO/ConsoleView/Program.cs
@@ -9,9 +9,55 @@ namespace ConsoleView
 {
     internal class Program
     {
+        static Logic logic = new Logic();
         static void Main(string[] args)
         {
-            Console.WriteLine("Nothing!");
+            while (true)
+            {
+                Console.Clear();
+
+                Console.WriteLine("DecanatPRO");
+                Console.WriteLine("1. Добавить нового студента");
+                Console.WriteLine("2. Удалить студента");
+                Console.WriteLine("3. Вывести весь список в таблицу");
+                Console.WriteLine("4. Вывести гистограмму: распределение студентов по специальностям");
+                Console.WriteLine("0. Выход");
+
+                Console.WriteLine("\nВыберите действие: ");
+                string choice = Console.ReadLine();
+
+                Console.Clear();
+
+                if (choice == "1")
+                {
+                    AddStudent();
+                }
+                else if (choice == "2")
+                {
+                    DeleteStudent();
+                }
+                else if (choice == "3")
+                {
+                    ListAllStudents();
+                }
+                else if (choice == "4")
+                {
+                    ShowHistogram();
+                }
+                else if (choice == "0")
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Выберите из один предложенных пунктов!");
+                }
+
+                Console.WriteLine("\nНажмите любую клавишу...");
+                Console.ReadKey();
+            }
         }
+
+
     }
 }
