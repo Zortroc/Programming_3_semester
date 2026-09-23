55971938c4281ff8ff924dea7ea6fc691bc0775e
Zortroc <zaharovilya2007@gmail.com>
2026-09-23 20:32:00 +0700

    закомментил прогу в ConsoleView

--- 1_laba/DecanatPRO/ConsoleView/Program.cs
+++ 1_laba/DecanatPRO/ConsoleView/Program.cs
@@ -9,13 +9,13 @@ namespace ConsoleView
 {
     internal class Program
     {
+        // создаём один объект Logic, в к-ом хранится список студентов и методы
         static Logic logic = new Logic();
         static void Main(string[] args)
         {
             while (true)
             {
-                Console.Clear();
-
+                // прописываем цикл для менюшки
                 Console.WriteLine("DecanatPRO");
                 Console.WriteLine("1. Добавить нового студента");
                 Console.WriteLine("2. Удалить студента");
@@ -26,6 +26,7 @@ namespace ConsoleView
                 Console.WriteLine("\nВыберите действие: ");
                 string choice = Console.ReadLine();
 
+                // очищаем консоль, чтобы результат блы на новом экране 
                 Console.Clear();
 
                 if (choice == "1")
@@ -53,11 +54,13 @@ namespace ConsoleView
                     Console.WriteLine("Выберите один из предложенных пунктов!");
                 }
 
+                // после выполнения действия прога не сразу вернется в меню, поэтому требуем инпута от пользователя
                 Console.WriteLine("\nНажмите любую клавишу...");
                 Console.ReadKey();
             }
         }
 
+        // прописываем методы по очереди
         static void AddStudent()
         {
             Console.WriteLine("Добавление нового студента...");
@@ -71,12 +74,14 @@ namespace ConsoleView
             Console.Write("Введите группу: ");
             string group = Console.ReadLine();
 
+            // передаем данные в бизнес-логику
+            // именно Logic создает объект Student и добавляет его в список students
             logic.AddStudent(name, speciality, group);
 
             Console.WriteLine("\nСтудент добавлен");
         }
 
-        static void DeleteStudetn()
+        static void DeleteStudent()
         {
             Console.WriteLine("Удалить студента...");
            
@@ -89,28 +94,38 @@ namespace ConsoleView
             Console.Write("Введите группу: ");
             string group = Console.ReadLine();
 
+            // снова передаем данные в логику
+            // внутри Logic прога находит нужного студента, а затем удаляет его
             logic.DeleteStudent(name, speciality, group);
 
             Console.WriteLine("\nСтудент удален");
         }
 
         static void ListAllStudents()
-        {
-            List<Student> allStudents = logic.ListAllStudents(); // короче тут ошибка, потому что компилятор не видит ссылку на Student, который берется из класса Model, но прикол в том, что нам нельзя делать связь ConsoleView -> Model, поэтому надо подумать что сделать
+        { // короче тут ошибка, потому что компилятор не видит ссылку на Student,
+          // который берется из класса Model, но прикол в том,
+          // что нам нельзя делать связь ConsoleView -> Model, поэтому надо подумать что сделать
+            List<Student> allStudents = logic.ListAllStudents();
         }
 
         static void ShowHistogram()
         {
+            // вызываем метод ShowHistogram из Logic
+            // он возвращает лист, то есть весь список данных для студента
             List<string> histogram = logic.ShowHistogram();
 
+            // если список пустой, значит студентов ещё нет и строить статистику не из чего
             if (histogram.Count == 0)
             {
                 Console.WriteLine("Гистограмму невозможно построить, список пуст");
                 return;
             }
 
-            Console.WriteLine("Распределение студентов по специальностям...");
+            Console.WriteLine("Распределение студентов по специальностям...\n");
 
+            // перебираем строки, к-ые вернул Logic
+            // например, сначала line = "ИТ: 3",
+            // затем line = "Дизайн: 1"
             foreach (string line in histogram)
             {
                 Console.WriteLine(line);
